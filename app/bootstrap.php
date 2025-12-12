3a25b205eec8f24293223dd7790aaf1acface440
Amin El-Rifai <amin@users.noreply.github.com>
2025-12-12 21:29:26 +0100

    Rename of constant HOTEL_FEATURES to AVAILABLE_FEATURES

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,6 +1,6 @@
 <?php
 
-define('HOTEL_FEATURES', json_decode(file_get_contents(__DIR__ . '/../database/features.json'), true));
+define('AVAILABLE_FEATURES', json_decode(file_get_contents(__DIR__ . '/../database/features.json'), true));
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
 $dotenv->safeLoad();
