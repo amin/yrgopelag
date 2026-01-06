2567efb6a1107675c75fd7a078deff202032d634
Amin El-Rifai <amin@users.noreply.github.com>
2026-01-06 18:38:21 +0100

    Add error reporting if dev enviroment

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+$dotenv->safeLoad();
+
 
 if (($_ENV['DEV'] ?? '') === '1') {
     ini_set('display_errors', '1');
@@ -7,15 +10,13 @@ if (($_ENV['DEV'] ?? '') === '1') {
     error_reporting(E_ALL);
 }
 
-
 define('AVAILABLE_FEATURES', json_decode(file_get_contents(__DIR__ . '/../database/features.json'), true));
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->safeLoad();
+foreach (glob(__DIR__ . '/utils/*.php') as $file) {
+    require_once $file;
+}
 
-require_once __DIR__ . '/db.php';
-
-foreach (glob(__DIR__ . '/helpers/*.php') as $file) {
+foreach (glob(__DIR__ . '/services/*.php') as $file) {
     require_once $file;
 }
 
