401d7b52a3a6e3dfc154f47bbd2c90a8b6417516
Amin El-Rifai <amin@users.noreply.github.com>
2026-01-06 19:13:22 +0100

    Use singelton pattern for database connection

--- app/utils/db.php
+++ app/utils/db.php
@@ -2,21 +2,27 @@
 
 declare(strict_types=1);
 
-$host = $_ENV['DB_HOST'] ?? 'db';
-$dbname = $_ENV['DB_NAME'];
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASSWORD'];
+function getDb(): PDO
+{
+    static $pdo = null;
 
-try {
-    $pdo = new PDO(
-        "pgsql:host=$host;dbname=$dbname",
-        $username,
-        $password,
-        [
-            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-        ]
-    );
-} catch (PDOException $e) {
-    die('Database connection failed: ' . $e->getMessage());
+    $host = $_ENV['DB_HOST'] ?? 'db';
+    $dbname = $_ENV['DB_NAME'];
+    $username = $_ENV['DB_USER'];
+    $password = $_ENV['DB_PASSWORD'];
+
+    if ($pdo === null) {
+        $pdo = new PDO(
+            "pgsql:host=$host;dbname=$dbname",
+            $username,
+            $password,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            ]
+        );
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    return $pdo;
 }
