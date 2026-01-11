13244bb80508261a8e4b6737f4d562e46dc46a13
Amin El-Rifai <amin@users.noreply.github.com>
2026-01-11 13:33:12 +0100

    Add try catch block to catch db exceptions

--- public/api/getBookings.php
+++ public/api/getBookings.php
@@ -6,4 +6,11 @@ require_once __DIR__ . '/../../vendor/autoload.php';
 require_once __DIR__ . '/../../app/bootstrap.php';
 
 header('Content-Type: Application/json');
-echo json_encode(getHotelCalendar());
+
+try {
+    echo json_encode(getHotelCalendar());
+} catch (PDOException $e) {
+    http_response_code(500);
+    echo json_encode(['error' => 'something went wrong']);
+    exit;
+}
